6b456951b5b029b396e8d5eee6898d6caf4c6f42
rob587 <robertocamm02@yahoo.com>
2026-04-09 14:54:18 +0200

    testing Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,48 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule, NgFor } from '@angular/common';
+
+interface Book {
+  nome: string;
+  autore: string;
+  pagine: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('app-libreria');
+  titolo = 'Libreria Ghebertz';
+  libri: Book[] = [
+    {
+      nome: 'harry potter',
+      autore: 'dubox',
+      pagine: 200,
+    },
+    {
+      nome: 'botolox',
+      autore: 'dugols',
+      pagine: 300,
+    },
+    {
+      nome: 'harry potter',
+      autore: 'peformance',
+      pagine: 400,
+    },
+  ];
+
+  contaLibri() {
+    return this.libri.length;
+  }
+
+  totalePagine() {
+    let sum = 0;
+    for (let i = 0; i < this.libri.length; i++) {
+      sum += this.libri[i].pagine;
+    }
+    return sum;
+  }
 }
